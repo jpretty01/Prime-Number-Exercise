dcfcd35a428c0a345521a05b4f5a56c2cf853679
Jeremy D. Pretty <70484698+jpretty01@users.noreply.github.com>
2021-12-23 22:22:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#Prime number exercise
+def prime_checker(number):
+  is_prime = True
+  for i in range(2, number):
+    if number & i == 0:
+      is_prime = False
+  if is_prime:
+    print("It is a prime number!")
+  else:
+    print("It is NOT a prime number!")
+
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
+
+
+
